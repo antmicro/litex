b41afc3ac8675bdf0a37f4ec78baf6d00b9a2a1c
Mateusz Hołenko <mholenko@antmicro.com>
2019-03-07 15:35:25 +0100

    [HACK] Disable uart tx interrupt.

--- litex/soc/software/libbase/uart.c
+++ litex/soc/software/libbase/uart.c
@@ -100,7 +100,7 @@ void uart_init(void)
 	tx_consume = 0;
 
 	uart_ev_pending_write(uart_ev_pending_read());
-	uart_ev_enable_write(UART_EV_TX | UART_EV_RX);
+	uart_ev_enable_write(/*UART_EV_TX |*/ UART_EV_RX);
 	irq_setmask(irq_getmask() | (1 << UART_INTERRUPT));
 }
 
