3df4545324e94a26d6e9fb939a68b090b3e48081
Pawel Czarnecki <pczarnecki@antmicro.com>
2020-11-27 17:53:37 +0100

    add synthetic buffers for synth IOs

--- litex/soc/cores/prm.py
+++ litex/soc/cores/prm.py
@@ -6,9 +6,34 @@ from litex.soc.interconnect import wishbone
 
 class PRIOInterfacer(Module):
     def __init__(self, pads):
-        self.bus = wishbone.Interface()
-        bus_io = self.bus.get_ios()
+        bus = wishbone.Interface()
+        bus_io = bus.get_ios()
 
         ###
 
-        self.comb += self.bus.connect_to_pads(pads=pads, mode="master")
+        self.comb += self.connect_to_pads(bus, pads, "master")
+
+    def connect_to_pads(self, bus, pads, mode="master"):
+        assert mode in ["slave", "master"]
+        r = []
+        for name, width, direction in bus.layout:
+            static_sig = Signal(width, name)
+            sig  = getattr(bus, name)
+            pad  = getattr(pads, name)
+            if mode == "master":
+                if direction == DIR_M_TO_S:
+                    for i in range(width):
+                        self.specials += Instance("SYN_OBUF", name=name+str(i), i_I=static_sig[i], o_O=sig[i])
+                    r.append(pad.eq(sig))
+                else:
+                    for i in range(width):
+                        self.specials += Instance("SYN_IBUF", name=name+str(i), i_I=pad, o_O=static_sig[i])
+                    r.append(sig.eq(pad))
+            else:
+                if direction == DIR_S_TO_M:
+                    r.append(pad.eq(sig))
+                else:
+                    r.append(sig.eq(pad))
+        print("connect_to_pads:")
+        print(r)
+        return r
