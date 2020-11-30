29e2752f0fd5c9a5b2620111f6f4ec589ee9e95f
Jan Kowalewski <jkowalewski@antmicro.com>
2020-11-30 15:36:33 +0100

    wip synth i/o to phys

--- litex/soc/cores/prm.py
+++ litex/soc/cores/prm.py
@@ -3,23 +3,24 @@
 from migen import *
 
 from litex.soc.interconnect import wishbone
+import sys
 
 class PRIOInterfacer(Module):
-    def __init__(self, pads):
+    def __init__(self, bus_ios, ins=None, outs=None):
         bus = wishbone.Interface()
         bus_io = bus.get_ios()
 
         ###
 
-        self.comb += self.connect_to_pads(bus, pads, "master")
+        self.comb += self.connect_to_pads(bus, bus_ios, ins, outs, "master")
 
-    def connect_to_pads(self, bus, pads, mode="master"):
+    def connect_to_pads(self, bus, bus_ios, ins=None, outs=None, mode="master"):
         assert mode in ["slave", "master"]
         r = []
         for name, width, direction in bus.layout:
             static_sig = Signal(width, name)
             sig  = getattr(bus, name)
-            pad  = getattr(pads, name)
+            pad  = getattr(bus_ios, name)
             if mode == "master":
                 if direction == DIR_M_TO_S:
                     for i in range(width):
@@ -34,6 +35,17 @@ class PRIOInterfacer(Module):
                     r.append(pad.eq(sig))
                 else:
                     r.append(sig.eq(pad))
+
+        if ins is not None:
+            for ipin in ins:
+                print(ipin)
+
+        if outs is not None:
+            for opin in outs:
+                print(opin)
+
+        sys.exit(1)
+
         print("connect_to_pads:")
         print(r)
         return r
