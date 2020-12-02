a0d2b8c1e8a4a838dbd8bcc59464837347b4639b
Jan Kowalewski <jkowalewski@antmicro.com>
2020-12-02 16:44:02 +0100

    Use top-level ports in SYN_BUFs
    Signed-off-by: Jan Kowalewski <jkowalewski@antmicro.com>

--- litex/soc/cores/prm.py
+++ litex/soc/cores/prm.py
@@ -6,32 +6,31 @@ from litex.soc.interconnect import wishbone
 
 class PRIOInterfacer(Module):
     def __init__(self, pads):
-        bus = wishbone.Interface()
-        bus_io = bus.get_ios()
+        self.bus = wishbone.Interface()
 
         ###
 
         # Connect bus signals to pads but also insert SYN_BUFS
-        self.comb += self.connect_to_pads(bus, pads, "master")
+        self.comb += self.connect_to_pads(pads, "master")
 
-    def connect_to_pads(self, bus, pads, mode="master"):
+    def connect_to_pads(self, pads, mode="master"):
         assert mode in ["slave", "master"]
         r = []
 
-        for name, width, direction in bus.layout:
+        for name, width, direction in self.bus.layout:
             static_sig = Signal(width, name)
-            sig  = getattr(bus, name)
+            sig  = getattr(self.bus, name)
             pad  = getattr(pads, name)
 
             if mode == "master":
                 if direction == DIR_M_TO_S:
                     for i in range(width):
-                        self.specials += Instance("SYN_OBUF", name=name+str(i), i_I=static_sig[i], o_O=sig[i])
-                    r.append(pad.eq(sig))
+                        self.specials += Instance("SYN_OBUF", name=name+str(i), i_I=sig[i], o_O=pad[i])
+                    #r.append(sig.eq(pad))
                 else:
                     for i in range(width):
-                        self.specials += Instance("SYN_IBUF", name=name+str(i), i_I=pad, o_O=static_sig[i])
-                    r.append(sig.eq(pad))
+                        self.specials += Instance("SYN_IBUF", name=name+str(i), i_I=pad[i], o_O=sig[i])
+                    #r.append(static_sig.eq(pad))
             else:
                 if direction == DIR_S_TO_M:
                     r.append(pad.eq(sig))
