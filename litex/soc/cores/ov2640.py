fbb871e0bc6c5d32be6684fd2f245fea7d79e207
Robert Szczepanski <rszczepanski@antmicro.com>
2021-12-27 19:16:20 +0100

    Use PCLK as clock signal

--- litex/soc/cores/ov2640.py
+++ litex/soc/cores/ov2640.py
@@ -1,7 +1,10 @@
 from migen import *
+from migen.genlib.cdc import MultiReg
+from migen.genlib.resetsync import AsyncResetSynchronizer
 
 from litex.soc.interconnect.csr import *
-from migen.genlib.cdc import MultiReg
+
+from litex.soc.cores.clock import *
 
 # OV2640 pads layout:
 # "data":  8
@@ -26,31 +29,40 @@ class OV2640(Module, AutoCSR):
         _href  = getattr(pads, "href")
         _vsync = getattr(pads, "vsync")
 
+        # Use PCLK as clock signal
+        self.clock_domains.cd_pclk = ClockDomain()
+        self.specials += [
+            Instance("IBUFG", i_I=_pclk, o_O=self.cd_pclk.clk),
+        ]
+        self.specials += AsyncResetSynchronizer(self.cd_pclk, ResetSignal())
+
+        self.comb += pclk[0].eq(self.cd_pclk.clk)
+
         # Connect pads to signals
         self.specials += [
-            MultiReg(_pclk, pclk[0]),
-            MultiReg(_href, href[0]),
-            MultiReg(_vsync, vsync[0]),
+            # MultiReg(self.cd_pclk.clk, pclk[0], odomain="pclk"),
+            MultiReg(_href, href[0], odomain="pclk"),
+            MultiReg(_vsync, vsync[0], odomain="pclk"),
         ]
         for i in range(len(data[3])):
-            self.specials += MultiReg(_data[i], data[0][i])
+            self.specials += MultiReg(_data[i], data[0][i], odomain="pclk")
 
         # Throw all signals into pipeline of 4 multiregs to stabilize tem
         for i in range(3):
             self.specials += [
-                MultiReg(href[i], href[i + 1]),
-                MultiReg(vsync[i], vsync[i + 1]),
+                MultiReg(href[i], href[i + 1], odomain="pclk"),
+                MultiReg(vsync[i], vsync[i + 1], odomain="pclk"),
             ]
 
             for j in range(len(data[3])):
-                self.specials += MultiReg(data[i][j], data[i + 1][j])
+                self.specials += MultiReg(data[i][j], data[i + 1][j], odomain="pclk")
 
         # Delayed vsync to check edge changes
-        self.specials += MultiReg(vsync[3], vsync[4]),
+        self.specials += MultiReg(vsync[3], vsync[4], odomain="pclk"),
 
         # For some reason pclk needs additional delay of 2 cycles so it's 6 in total
         for i in range(3 + delay_pclk):
-            self.specials += MultiReg(pclk[i], pclk[i + 1]),
+            self.specials += MultiReg(pclk[i], pclk[i + 1], odomain="pclk"),
 
         self.outer_valid  = outer_valid  = Signal()
         self.pixel_rgb565 = pixel_rgb565 = Signal(16)
@@ -61,17 +73,20 @@ class OV2640(Module, AutoCSR):
         counter     = Signal(32)
 
         # FSM
-        self.submodules.fsm = fsm = FSM(reset_state="WAIT_START_VSYNC")
+        self.submodules.fsm = fsm = ClockDomainsRenamer("pclk")(FSM(reset_state="WAIT_START_VSYNC"))
+
+        self.comb += inner_valid.eq(vsync[3] & href[3] & pclk[3 + delay_pclk] & pclk_valid)
 
         fsm.act("WAIT_START_VSYNC",
             If(vsync[3],
                 NextValue(pclk_valid, 0),
+                NextValue(outer_valid, 0),
                 NextValue(counter, 0),
                 NextState("CAPTURE")
             )
         )
         fsm.act("CAPTURE",
-            inner_valid.eq(vsync[3] & href[3] & pclk[3 + delay_pclk] & pclk_valid),
+            NextValue(outer_valid, 0),
             If (inner_valid,
                 If(counter == 0,
                     NextValue(first_pixel, 1),
@@ -79,14 +94,14 @@ class OV2640(Module, AutoCSR):
                 If(~counter & 1, #if counter % 2 == 0:
                     NextValue(last_data, data[3]),
                 ).Else(
-                    pixel_rgb565.eq((last_data << 8) | data[3]),
-                    pixel_rgba.eq(                                          # Save RGBA pixel as Little Endian (ABGR32)
+                    NextValue(pixel_rgb565, (last_data << 8) | data[3]),
+                    NextValue(pixel_rgba,                                   # Save RGBA pixel as Little Endian (ABGR32)
                         (last_data[3:] << 3) |                              # red   = pixel[11:16]
                         ((last_data[:3] << 5) | (data[3][5:] << 2)) << 8 |  # green = pixel[5:11]
                         (data[3][:5] << 3) << 16 |                          # blue  = pixel[:5]
                         0xff << 24                                          # alpha = 255 (not transparent)
                     ),
-                    outer_valid.eq(1),
+                    NextValue(outer_valid, 1),
                     NextValue(first_pixel, 0),
                 ),
                 NextValue(counter, counter + 1),
