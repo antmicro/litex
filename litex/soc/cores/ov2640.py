ce4446695dabcfb0541e6254653346ccc973d965
Robert Szczepanski <rszczepanski@antmicro.com>
2022-01-07 09:29:20 +0100

    ov2640: Add FIFO on output, fill it only when DMA expects data

--- litex/soc/cores/ov2640.py
+++ litex/soc/cores/ov2640.py
@@ -1,3 +1,4 @@
+from litex.soc.interconnect.stream import AsyncFIFO
 from migen import *
 from migen.genlib.cdc import MultiReg
 from migen.genlib.resetsync import AsyncResetSynchronizer
@@ -13,9 +14,10 @@ from litex.soc.cores.clock import *
 # "vsync": 1
 
 class OV2640(Module, AutoCSR):
-    def __init__(self, pads, mode="single_frame", delay_pclk=0):
+    def __init__(self, pads, dma_busy_signal, mode="single_frame", delay_pclk=0):
 
         assert mode in ["single_frame", "continous_stream"]
+        assert isinstance(dma_busy_signal, Signal)
 
         self.pclk_valid = pclk_valid = Signal()
 
@@ -34,7 +36,7 @@ class OV2640(Module, AutoCSR):
         self.specials += [
             Instance("IBUFG", i_I=_pclk, o_O=self.cd_pclk.clk),
         ]
-        self.specials += AsyncResetSynchronizer(self.cd_pclk, ResetSignal())
+        self.specials += AsyncResetSynchronizer(self.cd_pclk, ResetSignal("sys"))
 
         self.comb += pclk[0].eq(self.cd_pclk.clk)
 
@@ -68,59 +70,71 @@ class OV2640(Module, AutoCSR):
         self.pixel_rgb565 = pixel_rgb565 = Signal(16)
         self.pixel_rgba   = pixel_rgba   = Signal(32)
         self.first_pixel  = first_pixel  = Signal()
-        inner_valid = Signal()
+        self.inner_valid = inner_valid = Signal()
         last_data   = Signal(len(data[3]))
-        counter     = Signal(32)
+        self.counter = counter     = Signal(32)
 
         # FSM
-        self.submodules.fsm = fsm = ClockDomainsRenamer("pclk")(FSM(reset_state="WAIT_START_VSYNC"))
+        self.submodules.fsm = fsm = ClockDomainsRenamer("pclk")(FSM(reset_state="WAIT_START_DMA"))
+        self.submodules.fifo = fifo = ClockDomainsRenamer({"write": "pclk", "read": "sys"})(AsyncFIFO([("data", 32)], depth=512))
 
-        self.comb += inner_valid.eq(vsync[3] & href[3] & pclk[3 + delay_pclk] & pclk_valid)
+        # self.comb += inner_valid.eq(vsync[3] & href[3] & pclk[3 + delay_pclk] & pclk_valid)
 
-        fsm.act("WAIT_START_VSYNC",
-            If(vsync[3],
+        fsm.act("WAIT_START_DMA",
+            If(_vsync & dma_busy_signal,
                 NextValue(pclk_valid, 0),
-                NextValue(outer_valid, 0),
                 NextValue(counter, 0),
                 NextState("CAPTURE")
             )
         )
         fsm.act("CAPTURE",
+            # inner_valid.eq(vsync[3] & href[3] & pclk[3 + delay_pclk] & pclk_valid),
             NextValue(outer_valid, 0),
-            If (inner_valid,
+            NextValue(first_pixel, 0),
+            NextValue(fifo.sink.valid, 0),
+            If(~dma_busy_signal,
+                NextState("WAIT_START_DMA")
+            ),
+            If (_vsync & _href,
                 If(counter == 0,
                     NextValue(first_pixel, 1),
                 ),
                 If(~counter & 1, #if counter % 2 == 0:
-                    NextValue(last_data, data[3]),
+                    NextValue(last_data, _data),
                 ).Else(
-                    NextValue(pixel_rgb565, (last_data << 8) | data[3]),
+                    NextValue(pixel_rgb565, (last_data << 8) | data[0]),
                     NextValue(pixel_rgba,                                   # Save RGBA pixel as Little Endian (ABGR32)
                         (last_data[3:] << 3) |                              # red   = pixel[11:16]
-                        ((last_data[:3] << 5) | (data[3][5:] << 2)) << 8 |  # green = pixel[5:11]
-                        (data[3][:5] << 3) << 16 |                          # blue  = pixel[:5]
+                        ((last_data[:3] << 5) | (data[0][5:] << 2)) << 8 |  # green = pixel[5:11]
+                        (data[0][:5] << 3) << 16 |                          # blue  = pixel[:5]
                         0xff << 24                                          # alpha = 255 (not transparent)
                     ),
                     NextValue(outer_valid, 1),
-                    NextValue(first_pixel, 0),
+                    NextValue(fifo.sink.valid, 1),
+                    NextValue(fifo.sink.data,                               # Save RGBA pixel as Little Endian (ABGR32)
+                        (last_data[3:] << 3) |                              # red   = pixel[11:16]
+                        ((last_data[:3] << 5) | (_data[5:] << 2)) << 8 |  # green = pixel[5:11]
+                        (_data[:5] << 3) << 16 |                          # blue  = pixel[:5]
+                        0xff << 24
+                    ),
                 ),
                 NextValue(counter, counter + 1),
                 NextValue(pclk_valid, 0),
             ),
-            If(~pclk[3 + delay_pclk],
+            If(~pclk[0 + delay_pclk],
                 NextValue(pclk_valid, 1),
             ),
         )
 
         if mode == "single_frame":
             fsm.act("CAPTURE",
-                If(~vsync[3],
-                    NextState("WAIT_START_VSYNC")
+                If(~_vsync,
+                    NextState("WAIT_START_DMA")
                 )
             )
         elif mode == "continous_stream":
             fsm.act("CAPTURE",
-                If(~vsync[3],
+                If(~_vsync,
                     NextValue(pclk_valid, 0),
                     NextValue(counter, 0),
                 )
