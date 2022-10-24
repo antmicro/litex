67cdea55bf19835ae5d9639a295fd9894ee13abb
Maciej Dudek <mdudek@antmicro.com>
2022-10-24 16:00:43 +0200

    liblitedram: Codebase cleanup
    Moved PHY access functions to a separate file - accessors.h.
    Extracted common parts of algorithms.
    Added comments to `#endif`s indicating which `#if` ends there.
    
    Signed-off-by: Maciej Dudek <mdudek@antmicro.com>

---
+++ litex/soc/software/liblitedram/accessors.h
@@ -0,0 +1,142 @@
+#ifndef LIBLITEDRAM_ACCESSORS_H
+#define LIBLITEDRAM_ACCESSORS_H
+
+#include <generated/sdram_phy.h>
+
+typedef void (*action_callback)(int module);
+typedef void (*delay_callback)(int module, int dq_line, action_callback action);
+
+#ifdef SDRAM_PHY_READ_LEVELING_CAPABLE
+
+static void read_inc_dq_delay(int module) {
+	/* Increment delay */
+	ddrphy_rdly_dq_inc_write(1);
+}
+
+static void read_rst_dq_delay(int module) {
+	/* Reset delay */
+	ddrphy_rdly_dq_rst_write(1);
+}
+
+static void read_inc_dq_bitslip(int module) {
+	/* Increment bitslip */
+	ddrphy_rdly_dq_bitslip_write(1);
+}
+
+static void read_rst_dq_bitslip(int module) {
+	/* Reset bitslip */
+	ddrphy_rdly_dq_bitslip_rst_write(1);
+}
+
+#endif // SDRAM_PHY_READ_LEVELING_CAPABLE
+
+#ifdef SDRAM_PHY_WRITE_LEVELING_CAPABLE
+
+static void write_inc_dq_delay(int module) {
+	/* Increment DQ delay */
+	ddrphy_wdly_dq_inc_write(1);
+	cdelay(100);
+}
+
+static void write_rst_dq_delay(int module) {
+#if defined(SDRAM_PHY_USDDRPHY) || defined(SDRAM_PHY_USPDDRPHY)
+	/* Reset DQ delay */
+	while (ddrphy_wdly_dqs_inc_count_read() != 0) {
+		ddrphy_wdly_dq_inc_write(1);
+		cdelay(100);
+	}
+#else
+	/* Reset DQ delay */
+	ddrphy_wdly_dq_rst_write(1);
+	cdelay(100);
+#endif //defined(SDRAM_PHY_USDDRPHY) || defined(SDRAM_PHY_USPDDRPHY)
+}
+
+static void write_inc_dqs_delay(int module) {
+	/* Increment DQS delay */
+	ddrphy_wdly_dqs_inc_write(1);
+	cdelay(100);
+}
+
+static void write_rst_dqs_delay(int module) {
+#if defined(SDRAM_PHY_USDDRPHY) || defined(SDRAM_PHY_USPDDRPHY)
+	/* Reset DQS delay */
+	while (ddrphy_wdly_dqs_inc_count_read() != 0) {
+		ddrphy_wdly_dqs_inc_write(1);
+		cdelay(100);
+	}
+#else
+	/* Reset DQS delay */
+	ddrphy_wdly_dqs_rst_write(1);
+	cdelay(100);
+#endif //defined(SDRAM_PHY_USDDRPHY) || defined(SDRAM_PHY_USPDDRPHY)
+}
+
+
+static void write_inc_delay(int module) {
+	/* Increment DQ/DQS delay */
+	write_inc_dq_delay(module);
+	write_inc_dqs_delay(module);
+}
+
+static void write_rst_delay(int module) {
+	write_rst_dq_delay(module);
+	write_rst_dqs_delay(module);
+}
+
+#endif //SDRAM_PHY_WRITE_LEVELING_CAPABLE
+
+#ifdef SDRAM_PHY_WRITE_LATENCY_CALIBRATION_CAPABLE
+
+static void write_inc_dq_bitslip(int module) {
+	/* Increment bitslip */
+	ddrphy_wdly_dq_bitslip_write(1);
+}
+
+static void write_rst_dq_bitslip(int module) {
+	/* Increment bitslip */
+	ddrphy_wdly_dq_bitslip_rst_write(1);
+}
+
+#endif // SDRAM_PHY_WRITE_LATENCY_CALIBRATION_CAPABLE
+
+#if defined(SDRAM_PHY_WRITE_LEVELING_CAPABLE) || defined(SDRAM_PHY_WRITE_LATENCY_CALIBRATION_CAPABLE) || defined(SDRAM_PHY_READ_LEVELING_CAPABLE)
+
+static void sdram_select(int module, int dq_line) {
+	ddrphy_dly_sel_write(1 << module);
+
+#ifdef SDRAM_DELAY_PER_DQ
+	/* Select DQ line */
+	ddrphy_dq_dly_sel_write(1 << dq_line);
+#endif //SDRAM_DELAY_PER_DQ
+}
+
+static void sdram_deselect(int module, int dq_line) {
+	ddrphy_dly_sel_write(0);
+
+#ifdef SDRAM_DELAY_PER_DQ
+	/* Un-select DQ line */
+	ddrphy_dq_dly_sel_write(0);
+#endif //SDRAM_DELAY_PER_DQ
+
+#if defined(SDRAM_PHY_ECP5DDRPHY) || defined(SDRAM_PHY_GW2DDRPHY)
+	/* Sync all DQSBUFM's, By toggling all dly_sel (DQSBUFM.PAUSE) lines. */
+	ddrphy_dly_sel_write(0xff);
+	ddrphy_dly_sel_write(0);
+#endif //SDRAM_PHY_ECP5DDRPHY
+}
+
+static void sdram_leveling_action(int module, int dq_line, action_callback action) {
+	/* Select module */
+	sdram_select(module, dq_line);
+
+	/* Action */
+	action(module);
+
+	/* Un-select module */
+	sdram_deselect(module, dq_line);
+}
+
+#endif // defined(SDRAM_PHY_WRITE_LEVELING_CAPABLE) || defined(SDRAM_PHY_WRITE_LATENCY_CALIBRATION_CAPABLE) || defined(SDRAM_PHY_READ_LEVELING_CAPABLE)
+
+#endif
