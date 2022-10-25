008c693df1b25f3f461a82e33ecfd81bf489a2f5
Maciej Dudek <mdudek@antmicro.com>
2022-10-25 12:23:16 +0200

    DDR5 subchannels support
    Signed-off-by: Maciej Dudek <mdudek@antmicro.com>

--- litex/soc/software/liblitedram/accessors.h
+++ litex/soc/software/liblitedram/accessors.h
@@ -10,22 +10,50 @@ typedef void (*delay_callback)(int module, int dq_line, action_callback action);
 
 static void read_inc_dq_delay(int module) {
 	/* Increment delay */
+#ifdef SDRAM_PHY_SUBCHANNELS
+	if (module < SDRAM_PHY_MODULES/2)
+		ddrphy_A_rdly_dq_inc_write(1);
+	else
+		ddrphy_B_rdly_dq_inc_write(1);
+#else
 	ddrphy_rdly_dq_inc_write(1);
+#endif //SDRAM_PHY_SUBCHANNELS
 }
 
 static void read_rst_dq_delay(int module) {
 	/* Reset delay */
+#ifdef SDRAM_PHY_SUBCHANNELS
+	if (module < SDRAM_PHY_MODULES/2)
+		ddrphy_A_rdly_dq_rst_write(1);
+	else
+		ddrphy_B_rdly_dq_rst_write(1);
+#else
 	ddrphy_rdly_dq_rst_write(1);
+#endif //SDRAM_PHY_SUBCHANNELS
 }
 
 static void read_inc_dq_bitslip(int module) {
 	/* Increment bitslip */
+#ifdef SDRAM_PHY_SUBCHANNELS
+	if (module < SDRAM_PHY_MODULES/2)
+		ddrphy_A_rdly_dq_bitslip_write(1);
+	else
+		ddrphy_B_rdly_dq_bitslip_write(1);
+#else
 	ddrphy_rdly_dq_bitslip_write(1);
+#endif //SDRAM_PHY_SUBCHANNELS
 }
 
 static void read_rst_dq_bitslip(int module) {
 	/* Reset bitslip */
+#ifdef SDRAM_PHY_SUBCHANNELS
+	if (module < SDRAM_PHY_MODULES/2)
+		ddrphy_A_rdly_dq_bitslip_rst_write(1);
+	else
+		ddrphy_B_rdly_dq_bitslip_rst_write(1);
+#else
 	ddrphy_rdly_dq_bitslip_rst_write(1);
+#endif //SDRAM_PHY_SUBCHANNELS
 }
 
 #endif // SDRAM_PHY_READ_LEVELING_CAPABLE
@@ -90,12 +118,26 @@ static void write_rst_delay(int module) {
 
 static void write_inc_dq_bitslip(int module) {
 	/* Increment bitslip */
+#ifdef SDRAM_PHY_SUBCHANNELS
+	if (module < SDRAM_PHY_MODULES/2)
+		ddrphy_A_wdly_dq_bitslip_write(1);
+	else
+		ddrphy_B_wdly_dq_bitslip_write(1);
+#else
 	ddrphy_wdly_dq_bitslip_write(1);
+#endif //SDRAM_PHY_SUBCHANNELS
 }
 
 static void write_rst_dq_bitslip(int module) {
 	/* Increment bitslip */
+#ifdef SDRAM_PHY_SUBCHANNELS
+	if (module < SDRAM_PHY_MODULES/2)
+		ddrphy_A_wdly_dq_bitslip_rst_write(1);
+	else
+		ddrphy_B_wdly_dq_bitslip_rst_write(1);
+#else
 	ddrphy_wdly_dq_bitslip_rst_write(1);
+#endif //SDRAM_PHY_SUBCHANNELS
 }
 
 #endif // SDRAM_PHY_WRITE_LATENCY_CALIBRATION_CAPABLE
@@ -103,20 +145,48 @@ static void write_rst_dq_bitslip(int module) {
 #if defined(SDRAM_PHY_WRITE_LEVELING_CAPABLE) || defined(SDRAM_PHY_WRITE_LATENCY_CALIBRATION_CAPABLE) || defined(SDRAM_PHY_READ_LEVELING_CAPABLE)
 
 static void sdram_select(int module, int dq_line) {
+#ifdef SDRAM_PHY_SUBCHANNELS
+	if (module < SDRAM_PHY_MODULES/2)
+		ddrphy_A_dly_sel_write(1 << module);
+	else
+		ddrphy_B_dly_sel_write(1 << (module/2));
+#else
 	ddrphy_dly_sel_write(1 << module);
+#endif //SDRAM_PHY_SUBCHANNELS
 
 #ifdef SDRAM_DELAY_PER_DQ
 	/* Select DQ line */
+#ifdef SDRAM_PHY_SUBCHANNELS
+	if (module < SDRAM_PHY_MODULES/2)
+		ddrphy_A_dq_dly_sel_write(1 << dq_line);
+	else
+		ddrphy_B_dq_dly_sel_write(1 << dq_line);
+#else
 	ddrphy_dq_dly_sel_write(1 << dq_line);
+#endif //SDRAM_PHY_SUBCHANNELS
 #endif //SDRAM_DELAY_PER_DQ
 }
 
 static void sdram_deselectd(int module, int dq_line) {
+#ifdef SDRAM_PHY_SUBCHANNELS
+	if (module < SDRAM_PHY_MODULES/2)
+		ddrphy_A_dly_sel_write(0);
+	else
+		ddrphy_B_dly_sel_write(0);
+#else
 	ddrphy_dly_sel_write(0);
+#endif //SDRAM_PHY_SUBCHANNELS
 
 #ifdef SDRAM_DELAY_PER_DQ
 	/* Un-select DQ line */
+#ifdef SDRAM_PHY_SUBCHANNELS
+	if (module < SDRAM_PHY_MODULES/2)
+		ddrphy_A_dq_dly_sel_write(0);
+	else
+		ddrphy_B_dq_dly_sel_write(0);
+#else
 	ddrphy_dq_dly_sel_write(0);
+#endif //SRAM_PHY_SUBCHANNELS
 #endif //SDRAM_DELAY_PER_DQ
 
 #if defined(SDRAM_PHY_ECP5DDRPHY) || defined(SDRAM_PHY_GW2DDRPHY)
