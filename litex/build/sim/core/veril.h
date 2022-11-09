b1fa6d3461d241577037f7c23bf11b4702c94f34
Maciej Dudek <mdudek@antmicro.com>
2022-11-09 18:00:11 +0100

    Flush tfp file on SIGINT and exit
    Signed-off-by: Maciej Dudek <mdudek@antmicro.com>

--- litex/build/sim/core/veril.h
+++ litex/build/sim/core/veril.h
@@ -11,6 +11,7 @@ extern "C" uint64_t litex_sim_eval(void *vsim, uint64_t time_fs, uint64_t timeba
 extern "C" void litex_sim_init_tracer(void *vsim, long start, long end);
 extern "C" void litex_sim_tracer_dump();
 extern "C" int litex_sim_got_finish();
+extern "C" void litex_sim_trace_flush_and_close();
 #if VM_COVERAGE
 extern "C" void litex_sim_coverage_dump();
 #endif
@@ -19,6 +20,7 @@ uint64_t litex_sim_eval(void *vsim, uint64_t time_fs, uint64_t timebase_fs);
 void litex_sim_init_tracer(void *vsim);
 void litex_sim_tracer_dump();
 int litex_sim_got_finish();
+void litex_sim_trace_flush_and_close();
 void litex_sim_init_cmdargs(int argc, char *argv[]);
 #if VM_COVERAGE
 void litex_sim_coverage_dump();
