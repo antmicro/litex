9a31a60ebd2c292bf42024743b13e11eb0bdecef
Maciej Dudek <mdudek@antmicro.com>
2022-11-18 12:34:08 +0100

    DDR5 CS/CA training algorithm improvments.
    Signed-off-by: Maciej Dudek <mdudek@antmicro.com>

--- litex/soc/software/liblitedram/accessors.h
+++ litex/soc/software/liblitedram/accessors.h
@@ -1,399 +1,67 @@
+#include <generated/sdram_phy.h>
 #ifndef LIBLITEDRAM_ACCESSORS_H
 #define LIBLITEDRAM_ACCESSORS_H
 
-#include <generated/sdram_phy.h>
+extern int _sdram_write_leveling_bitslips[16];
+extern int _sdram_write_leveling_dat_delays[16];
 
 typedef void (*action_callback)(int module);
 typedef void (*delay_callback)(int module, int dq_line, action_callback action);
 
-int _sdram_write_leveling_bitslips[16];
-int _sdram_write_leveling_dat_delays[16];
-
 #if defined(SDRAM_PHY_READ_LEVELING_CAPABLE) || defined(SDRAM_INPUT_DELAY_CAPABLE)
 
-static void read_inc_dq_delay(int module) {
-	/* Increment delay */
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (module < SDRAM_PHY_MODULES/2)
-		ddrphy_A_rdly_dq_inc_write(1);
-	else
-		ddrphy_B_rdly_dq_inc_write(1);
-#else
-	ddrphy_rdly_dq_inc_write(1);
-#endif //SDRAM_PHY_SUBCHANNELS
-}
-
-static void read_rst_dq_delay(int module) {
-	/* Reset delay */
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (module < SDRAM_PHY_MODULES/2)
-		ddrphy_A_rdly_dq_rst_write(1);
-	else
-		ddrphy_B_rdly_dq_rst_write(1);
-#else
-	ddrphy_rdly_dq_rst_write(1);
-#endif //SDRAM_PHY_SUBCHANNELS
-}
-
-static void read_inc_dq_bitslip(int module) {
-	/* Increment bitslip */
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (module < SDRAM_PHY_MODULES/2)
-		ddrphy_A_rdly_dq_bitslip_write(1);
-	else
-		ddrphy_B_rdly_dq_bitslip_write(1);
-#else
-	ddrphy_rdly_dq_bitslip_write(1);
-#endif //SDRAM_PHY_SUBCHANNELS
-}
-
-static void read_rst_dq_bitslip(int module) {
-	/* Reset bitslip */
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (module < SDRAM_PHY_MODULES/2)
-		ddrphy_A_rdly_dq_bitslip_rst_write(1);
-	else
-		ddrphy_B_rdly_dq_bitslip_rst_write(1);
-#else
-	ddrphy_rdly_dq_bitslip_rst_write(1);
-#endif //SDRAM_PHY_SUBCHANNELS
-}
+void read_inc_dq_delay(int module);
+void read_rst_dq_delay(int module);
+void read_inc_dq_bitslip(int module);
+void read_rst_dq_bitslip(int module);
 
 #endif // defined(SDRAM_PHY_READ_LEVELING_CAPABLE) || defined(SDRAM_INPUT_DELAY_CAPABLE)
 
 #if defined(SDRAM_PHY_WRITE_LEVELING_CAPABLE) || defined(SDRAM_FULL_OUTPUT_DELAY_CAPABLE)
 
-static void write_inc_dq_delay(int module) {
-	/* Increment DQ delay */
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (module < SDRAM_PHY_MODULES/2)
-		ddrphy_A_wdly_dq_inc_write(1);
-	else
-		ddrphy_B_wdly_dq_inc_write(1);
-#else
-	ddrphy_wdly_dq_inc_write(1);
-#endif // SDRAM_PHY_SUBCHANNELS
-	cdelay(100);
-}
+void write_inc_dq_delay(int module);
+void write_rst_dq_delay(int module);
+void write_inc_dqs_delay(int module);
+void write_rst_dqs_delay(int module);
 
-static void write_rst_dq_delay(int module) {
-#if defined(SDRAM_PHY_USDDRPHY) || defined(SDRAM_PHY_USPDDRPHY)
-	/* Reset DQ delay */
-	while (ddrphy_wdly_dqs_inc_count_read() != 0) {
-		ddrphy_wdly_dq_inc_write(1);
-		cdelay(100);
-	}
-#else
-	/* Reset DQ delay */
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (module < SDRAM_PHY_MODULES/2)
-		ddrphy_A_wdly_dq_rst_write(1);
-	else
-		ddrphy_B_wdly_dq_rst_write(1);
-#else
-	ddrphy_wdly_dq_rst_write(1);
-#endif // SDRAM_PHY_SUBCHANNELS
-	cdelay(100);
-#endif //defined(SDRAM_PHY_USDDRPHY) || defined(SDRAM_PHY_USPDDRPHY)
-}
-
-static void write_inc_dqs_delay(int module) {
-	/* Increment DQS delay */
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (module < SDRAM_PHY_MODULES/2)
-		ddrphy_A_wdly_dqs_inc_write(1);
-	else
-		ddrphy_B_wdly_dqs_inc_write(1);
-#else
-	ddrphy_wdly_dqs_inc_write(1);
-#endif // SDRAM_PHY_SUBCHANNELS
-	cdelay(100);
-}
-
-static void write_rst_dqs_delay(int module) {
-#if defined(SDRAM_PHY_USDDRPHY) || defined(SDRAM_PHY_USPDDRPHY)
-	/* Reset DQS delay */
-	while (ddrphy_wdly_dqs_inc_count_read() != 0) {
-		ddrphy_wdly_dqs_inc_write(1);
-		cdelay(100);
-	}
-#else
-	/* Reset DQS delay */
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (module < SDRAM_PHY_MODULES/2)
-		ddrphy_A_wdly_dqs_rst_write(1);
-	else
-		ddrphy_B_wdly_dqs_rst_write(1);
-#else
-	ddrphy_wdly_dqs_rst_write(1);
-#endif // SDRAM_PHY_SUBCHANNELS
-	cdelay(100);
-#endif //defined(SDRAM_PHY_USDDRPHY) || defined(SDRAM_PHY_USPDDRPHY)
-}
-
-
-static void write_inc_delay(int module) {
-	/* Increment DQ/DQS delay */
-	write_inc_dq_delay(module);
-	write_inc_dqs_delay(module);
-}
-
-static void write_rst_delay(int module) {
-	write_rst_dq_delay(module);
-	write_rst_dqs_delay(module);
-}
+void write_inc_delay(int module);
+void write_rst_delay(int module);
 
 #endif // defined(SDRAM_PHY_WRITE_LEVELING_CAPABLE) || defined(SDRAM_FULL_OUTPUT_DELAY_CAPABLE)
 
 #if defined(SDRAM_PHY_WRITE_LATENCY_CALIBRATION_CAPABLE) || defined(SDRAM_BITSLIP_CAPABLE)
 
-static void write_inc_dq_bitslip(int module) {
-	/* Increment bitslip */
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (module < SDRAM_PHY_MODULES/2)
-		ddrphy_A_wdly_dq_bitslip_write(1);
-	else
-		ddrphy_B_wdly_dq_bitslip_write(1);
-#else
-	ddrphy_wdly_dq_bitslip_write(1);
-#endif //SDRAM_PHY_SUBCHANNELS
-}
-
-static void write_rst_dq_bitslip(int module) {
-	/* Increment bitslip */
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (module < SDRAM_PHY_MODULES/2)
-		ddrphy_A_wdly_dq_bitslip_rst_write(1);
-	else
-		ddrphy_B_wdly_dq_bitslip_rst_write(1);
-#else
-	ddrphy_wdly_dq_bitslip_rst_write(1);
-#endif //SDRAM_PHY_SUBCHANNELS
-}
-
-static void write_inc_dqs_bitslip(int module) {
-	/* Increment bitslip */
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (module < SDRAM_PHY_MODULES/2)
-		ddrphy_A_wdly_dqs_bitslip_write(1);
-	else
-		ddrphy_B_wdly_dqs_bitslip_write(1);
-#else
-	ddrphy_wdly_dqs_bitslip_write(1);
-#endif //SDRAM_PHY_SUBCHANNELS
-}
-
-static void write_rst_dqs_bitslip(int module) {
-	/* Increment bitslip */
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (module < SDRAM_PHY_MODULES/2)
-		ddrphy_A_wdly_dqs_bitslip_rst_write(1);
-	else
-		ddrphy_B_wdly_dqs_bitslip_rst_write(1);
-#else
-	ddrphy_wdly_dqs_bitslip_rst_write(1);
-#endif //SDRAM_PHY_SUBCHANNELS
-}
+void write_inc_dq_bitslip(int module);
+void write_rst_dq_bitslip(int module);
+void write_inc_dqs_bitslip(int module);
+void write_rst_dqs_bitslip(int module);
 
 #endif // defined(SDRAM_PHY_WRITE_LATENCY_CALIBRATION_CAPABLE) || defined(SDRAM_BITSLIP_CAPABLE)
 
 #if defined(SDRAM_FULL_OUTPUT_DELAY_CAPABLE) || defined(SDRAM_PHY_ADDRESS_DELAY_CAPABLE)
 
-static void cs_inc_delay(int channel) {
-	/* Increment CS delay */
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (channel)
-		ddrphy_B_csdly_inc_write(1);
-	else
-		ddrphy_A_csdly_inc_write(1);
-#else
-	ddrphy_csdly_inc_write(1);
-#endif //SDRAM_PHY_SUBCHANNELS
-
-}
-
-static void cs_rst_delay(int channel) {
-	/* Reset CS delay */
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (channel)
-		ddrphy_B_csdly_rst_write(1);
-	else
-		ddrphy_A_csdly_rst_write(1);
-#else
-	ddrphy_csdly_rst_write(1);
-#endif //SDRAM_PHY_SUBCHANNELS
-
-}
-
-static void ca_inc_delay(int channel) {
-	/* Increment CA delay */
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (channel)
-		ddrphy_B_cadly_inc_write(1);
-	else
-		ddrphy_A_cadly_inc_write(1);
-#else
-	ddrphy_cadly_inc_write(1);
-#endif //SDRAM_PHY_SUBCHANNELS
-
-}
-
-static void ca_rst_delay(int channel) {
-	/* Reset CA delay */
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (channel)
-		ddrphy_B_cadly_rst_write(1);
-	else
-		ddrphy_A_cadly_rst_write(1);
-#else
-	ddrphy_cadly_rst_write(1);
-#endif //SDRAM_PHY_SUBCHANNELS
-}
-
-static void par_inc_delay(int channel) {
-	/* Increment PAR delay */
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (channel)
-		ddrphy_B_pardly_inc_write(1);
-	else
-		ddrphy_A_pardly_inc_write(1);
-#else
-	ddrphy_pardly_inc_write(1);
-#endif //SDRAM_PHY_SUBCHANNELS
-
-}
-
-static void par_rst_delay(int channel) {
-	/* Reset PAR delay */
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (channel)
-		ddrphy_B_pardly_rst_write(1);
-	else
-		ddrphy_A_pardly_rst_write(1);
-#else
-	ddrphy_pardly_rst_write(1);
-#endif //SDRAM_PHY_SUBCHANNELS
-}
-
-static void cs_ca_select(int channel, int ca_cs_line) {
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (channel)
-		ddrphy_B_dly_sel_write(1 << ca_cs_line);
-	else
-		ddrphy_A_dly_sel_write(1 << ca_cs_line);
-#else
-	ddrphy_dly_sel_write(1 << ca_cs_line);
-#endif //SDRAM_PHY_SUBCHANNELS
-}
-
-static void cs_ca_deselect(int channel, int ca_cs_line) {
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (channel)
-		ddrphy_B_dly_sel_write(0);
-	else
-		ddrphy_A_dly_sel_write(0);
-#else
-	ddrphy_dly_sel_write(0);
-#endif //SDRAM_PHY_SUBCHANNELS
-
-#if defined(SDRAM_PHY_ECP5DDRPHY) || defined(SDRAM_PHY_GW2DDRPHY)
-	/* Sync all DQSBUFM's, By toggling all dly_sel (DQSBUFM.PAUSE) lines. */
-	ddrphy_dly_sel_write(0xff);
-	ddrphy_dly_sel_write(0);
-#endif //SDRAM_PHY_ECP5DDRPHY
-}
+void cs_inc_delay(int channel);
+void cs_rst_delay(int channel);
+void ca_inc_delay(int channel);
+void ca_rst_delay(int channel);
+void par_inc_delay(int channel);
+void par_rst_delay(int channel);
+void cs_ca_select(int channel, int ca_cs_line);
+void cs_ca_deselect(int channel, int ca_cs_line);
 
 #endif // defined(SDRAM_FULL_OUTPUT_DELAY_CAPABLE) || defined(SDRAM_PHY_ADDRESS_DELAY_CAPABLE)
 
 #if defined(SDRAM_PHY_WRITE_LEVELING_CAPABLE) || defined(SDRAM_PHY_WRITE_LATENCY_CALIBRATION_CAPABLE) || defined(SDRAM_PHY_READ_LEVELING_CAPABLE) || \
 defined(SDRAM_FULL_OUTPUT_DELAY_CAPABLE) || defined(SDRAM_INPUT_DELAY_CAPABLE) || defined(SDRAM_BITSLIP_CAPABLE)
 
-static void sdram_select(int module, int dq_line) {
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (module < SDRAM_PHY_MODULES/2)
-		ddrphy_A_dly_sel_write(1 << module);
-	else
-		ddrphy_B_dly_sel_write(1 << (module/2));
-#else
-	ddrphy_dly_sel_write(1 << module);
-#endif //SDRAM_PHY_SUBCHANNELS
-
-#ifdef SDRAM_DELAY_PER_DQ
-	/* Select DQ line */
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (module < SDRAM_PHY_MODULES/2)
-		ddrphy_A_dq_dly_sel_write(1 << dq_line);
-	else
-		ddrphy_B_dq_dly_sel_write(1 << dq_line);
-#else
-	ddrphy_dq_dly_sel_write(1 << dq_line);
-#endif //SDRAM_PHY_SUBCHANNELS
-#endif //SDRAM_DELAY_PER_DQ
-}
-
-static void sdram_deselect(int module, int dq_line) {
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (module < SDRAM_PHY_MODULES/2)
-		ddrphy_A_dly_sel_write(0);
-	else
-		ddrphy_B_dly_sel_write(0);
-#else
-	ddrphy_dly_sel_write(0);
-#endif //SDRAM_PHY_SUBCHANNELS
-
-#ifdef SDRAM_DELAY_PER_DQ
-	/* Un-select DQ line */
-#ifdef SDRAM_PHY_SUBCHANNELS
-	if (module < SDRAM_PHY_MODULES/2)
-		ddrphy_A_dq_dly_sel_write(0);
-	else
-		ddrphy_B_dq_dly_sel_write(0);
-#else
-	ddrphy_dq_dly_sel_write(0);
-#endif //SRAM_PHY_SUBCHANNELS
-#endif //SDRAM_DELAY_PER_DQ
-
-#if defined(SDRAM_PHY_ECP5DDRPHY) || defined(SDRAM_PHY_GW2DDRPHY)
-	/* Sync all DQSBUFM's, By toggling all dly_sel (DQSBUFM.PAUSE) lines. */
-	ddrphy_dly_sel_write(0xff);
-	ddrphy_dly_sel_write(0);
-#endif //SDRAM_PHY_ECP5DDRPHY
-}
-
-static void sdram_leveling_action(int module, int dq_line, action_callback action) {
-	/* Select module */
-	sdram_select(module, dq_line);
-
-	/* Action */
-	action(module);
-
-	/* Un-select module */
-	sdram_deselect(module, dq_line);
-}
+void sdram_select(int module, int dq_line);
+void sdram_deselect(int module, int dq_line);
+void sdram_leveling_action(int module, int dq_line, action_callback action);
 
 #endif // defined(SDRAM_PHY_WRITE_LEVELING_CAPABLE) || defined(SDRAM_PHY_WRITE_LATENCY_CALIBRATION_CAPABLE) || defined(SDRAM_PHY_READ_LEVELING_CAPABLE) ...
 
-void sdram_write_leveling_rst_dat_delay(int module, int show) {
-	_sdram_write_leveling_dat_delays[module] = -1;
-	if (show)
-		printf("Reseting Dat delay of module %d\n", module);
-}
-
-void sdram_write_leveling_force_dat_delay(int module, int taps, int show) {
-	_sdram_write_leveling_dat_delays[module] = taps;
-	if (show)
-		printf("Forcing Dat delay of module %d to %d taps\n", module, taps);
-}
-
-void sdram_write_leveling_rst_bitslip(int module, int show) {
-	_sdram_write_leveling_bitslips[module] = -1;
-	if (show)
-		printf("Reseting Bitslip of module %d\n", module);
-}
-
-void sdram_write_leveling_force_bitslip(int module, int bitslip, int show) {
-	_sdram_write_leveling_bitslips[module] = bitslip;
-	if (show)
-		printf("Forcing Bitslip of module %d to %d\n", module, bitslip);
-}
+void sdram_write_leveling_rst_dat_delay(int module, int show);
+void sdram_write_leveling_force_dat_delay(int module, int taps, int show);
+void sdram_write_leveling_rst_bitslip(int module, int show);
+void sdram_write_leveling_force_bitslip(int module, int bitslip, int show);
 #endif
---
+++ litex/soc/software/liblitedram/ddr5_helpers.c
@@ -0,0 +1,269 @@
+#include <liblitedram/accessors.h>
+#include <liblitedram/ddr5_helpers.h>
+#include <stdio.h>
+
+#ifdef MEMORY_TYPE_DDR5
+int prep_payload (int cs, int command, int wrdata_en,
+                  int wrdata_mask, int rddata_en) {
+    int payload;
+#ifdef SDRAM_PHY_SUBCHANNELS
+    payload = cs << CSR_SDRAM_DFII_A_CMDINJECTOR_COMMAND_STORAGE_CS_OFFSET | \
+              command << CSR_SDRAM_DFII_A_CMDINJECTOR_COMMAND_STORAGE_CA_OFFSET | \
+              wrdata_en << CSR_SDRAM_DFII_A_CMDINJECTOR_COMMAND_STORAGE_WRDATA_EN_OFFSET | \
+              wrdata_mask << CSR_SDRAM_DFII_A_CMDINJECTOR_COMMAND_STORAGE_WRDATA_MASK_OFFSET | \
+              rddata_en << CSR_SDRAM_DFII_A_CMDINJECTOR_COMMAND_STORAGE_RDDATA_EN_OFFSET;
+#else
+    payload = cs << CSR_SDRAM_DFII_CMDINJECTOR_COMMAND_STORAGE_CS_OFFSET | \
+              command << CSR_SDRAM_DFII_CMDINJECTOR_COMMAND_STORAGE_CA_OFFSET | \
+              wrdata_en << CSR_SDRAM_DFII_CMDINJECTOR_COMMAND_STORAGE_WRDATA_EN_OFFSET | \
+              wrdata_mask << CSR_SDRAM_DFII_CMDINJECTOR_COMMAND_STORAGE_WRDATA_MASK_OFFSET | \
+              rddata_en << CSR_SDRAM_DFII_CMDINJECTOR_COMMAND_STORAGE_RDDATA_EN_OFFSET;
+#endif
+    return payload;
+}
+
+void upload_payload(int channel, int phases, int payload) {
+#ifdef SDRAM_PHY_SUBCHANNELS
+    if(channel) {
+        sdram_dfii_B_cmdinjector_command_storage_write(payload);
+        sdram_dfii_B_cmdinjector_phase_addr_write(phases);
+    } else {
+        sdram_dfii_A_cmdinjector_command_storage_write(payload);
+        sdram_dfii_A_cmdinjector_phase_addr_write(phases);
+    }
+#else
+    sdram_dfii_cmdinjector_command_storage_write(payload);
+    sdram_dfii_cmdinjector_phase_addr_write(phases);
+#endif
+}
+
+void store_payload(int channel, int single) {
+#ifdef SDRAM_PHY_SUBCHANNELS
+    if(channel) {
+        if (single == 0)
+            sdram_dfii_cmdinjector_store_continuous_cmd_write(1);
+        else
+            sdram_dfii_cmdinjector_store_singleshot_cmd_write(1);
+    } else {
+        if (single == 0)
+            sdram_dfii_cmdinjector_store_continuous_cmd_write(1);
+        else
+            sdram_dfii_cmdinjector_store_singleshot_cmd_write(1);
+    }
+#else
+    if (single == 0)
+        sdram_dfii_cmdinjector_store_continuous_cmd_write(1);
+    else
+        sdram_dfii_cmdinjector_store_singleshot_cmd_write(1);
+#endif
+}
+
+void cmd_injector(int channel, int phases, int cs, int command,
+                         int wrdata_en, int wrdata_mask, int rddata_en, int single) {
+    int payload = prep_payload(cs, command, wrdata_en, wrdata_mask, rddata_en);
+    upload_payload(channel, phases, payload);
+    store_payload(channel, single);
+}
+
+void issue_single(int channel) {
+#ifdef SDRAM_PHY_SUBCHANNELS
+    if(channel) {
+        sdram_dfii_B_cmdinjector_single_shot_write(1);
+        sdram_dfii_B_cmdinjector_issue_command_write(1);
+        sdram_dfii_B_cmdinjector_single_shot_write(0);
+    } else {
+        sdram_dfii_A_cmdinjector_single_shot_write(1);
+        sdram_dfii_A_cmdinjector_issue_command_write(1);
+        sdram_dfii_A_cmdinjector_single_shot_write(0);
+    }
+#else
+    sdram_dfii_cmdinjector_single_shot_write(1);
+    sdram_dfii_cmdinjector_issue_command_write(1);
+    sdram_dfii_cmdinjector_single_shot_write(0);
+#endif
+}
+
+void setup_capture(int channel, int setup) {
+#ifdef SDRAM_PHY_SUBCHANNELS
+    if(channel) {
+        sdram_dfii_B_cmdinjector_sample_write(0);
+        sdram_dfii_B_cmdinjector_setup_write(setup);
+        sdram_dfii_B_cmdinjector_reset_write(1);
+    } else {
+        sdram_dfii_A_cmdinjector_sample_write(0);
+        sdram_dfii_A_cmdinjector_setup_write(setup);
+        sdram_dfii_A_cmdinjector_reset_write(1);
+    }
+#else
+    sdram_dfii_cmdinjector_sample_write(0);
+    sdram_dfii_cmdinjector_setup_write(setup);
+    sdram_dfii_cmdinjector_reset_write(1);
+#endif
+}
+
+void start_capture(int channel) {
+#ifdef SDRAM_PHY_SUBCHANNELS
+    if(channel) {
+        sdram_dfii_B_cmdinjector_sample_write(1);
+    } else {
+        sdram_dfii_A_cmdinjector_sample_write(1);
+    }
+#else
+    sdram_dfii_cmdinjector_sample_write(1);
+#endif
+}
+
+void stop_capture(int channel) {
+#ifdef SDRAM_PHY_SUBCHANNELS
+    if(channel) {
+        sdram_dfii_B_cmdinjector_sample_write(0);
+    } else {
+        sdram_dfii_A_cmdinjector_sample_write(0);
+    }
+#else
+    sdram_dfii_cmdinjector_sample_write(0);
+#endif
+}
+
+uint32_t capture_result(int channel) {
+#ifdef SDRAM_PHY_SUBCHANNELS
+    if(channel) {
+        return sdram_dfii_B_cmdinjector_result_read();
+    } else {
+        return sdram_dfii_A_cmdinjector_result_read();
+    }
+#else
+    return sdram_dfii_cmdinjector_result_read();
+#endif
+}
+
+int or_sample(int channel) {
+    setup_capture(channel, 0);
+    cdelay(100);
+    start_capture(channel);
+    cdelay(2000);
+    stop_capture(channel);
+    return !capture_result(channel);
+}
+
+int and_sample(int channel) {
+    setup_capture(channel, 3);
+    cdelay(100);
+    start_capture(channel);
+    cdelay(2000);
+    stop_capture(channel);
+    return !!capture_result(channel);
+}
+
+void disable_2n_mode(void) {
+    int value = sdram_dfii_control_read();
+    value &= ~DFII_CONTROL_2N_MODE;
+    sdram_dfii_control_write(value);
+    printf("Switching to 1N mode\n");
+}
+
+void enable_2n_mode(void) {
+    int value = sdram_dfii_control_read();
+    value |= DFII_CONTROL_2N_MODE;
+    sdram_dfii_control_write(value);
+    printf("Switching to 2N mode\n");
+}
+
+void cs_rst(int channel, int rank, int address) {
+    cs_ca_select(channel, rank);
+    cs_rst_delay(channel);
+    cs_ca_deselect(channel, rank);
+}
+
+void cs_inc(int channel, int rank, int address) {
+    cs_ca_select(channel, rank);
+    cs_inc_delay(channel);
+    cs_ca_deselect(channel, rank);
+}
+
+void ca_rst(int channel, int rank, int address) {
+    cs_ca_select(channel, address);
+    ca_rst_delay(channel);
+    cs_ca_deselect(channel, address);
+}
+
+void ca_inc(int channel, int rank, int address) {
+    cs_ca_select(channel, address);
+    ca_inc_delay(channel);
+    cs_ca_deselect(channel, address);
+}
+
+void enter_cs(int channel, int rank) {
+    cmd_injector(channel, 0xf, 0, 0xf | (1<<5), 0, 0, 0, 0);
+    cmd_injector(channel, 0xf, 1<<rank, 0xf | (1<<5), 0, 0, 0, 0);
+    cmd_injector(channel, 0xf, 0, 0xf | (1<<5), 0, 0, 0, 0);
+    cdelay(100);
+}
+
+void exit_cs(int channel, int rank) {
+    cmd_injector(channel, 0xf, 0, 0xf, 0, 0, 0, 0);
+    cmd_injector(channel, 0xf, 1<<rank, 0xf, 0, 0, 0, 0);
+    cmd_injector(channel, 0xf, 0, 0xf, 0, 0, 0, 0);
+    cdelay(100);
+}
+
+void cs_sample_prep(int channel, int rank, int address, int l2h) {
+    cmd_injector(channel, 0xf, 0, 0x1f, 0, 0, 0, 0);
+    cmd_injector(channel, 0xa>>l2h, 1<<rank, 0x1f, 0, 0, 0, 0);
+    cdelay(100);
+}
+
+void enter_ca(int channel, int rank) {
+    cmd_injector(channel, 0xf, 0, 0xf | (3<<5), 0, 0, 0, 0);
+    cmd_injector(channel, 0xf, 1<<rank, 0xf | (3<<5), 0, 0, 0, 0);
+    cmd_injector(channel, 0xf, 0, 0xf | (3<<5), 0, 0, 0, 0);
+    cdelay(100);
+}
+
+void exit_ca(int channel, int rank) {
+    cmd_injector(channel, 0xff, 1<<rank, 0x1f, 0, 0, 0, 1);
+    issue_single(channel);
+    cdelay(100);
+}
+
+void ca_sample_prep_current_period(int channel, int rank, int address, int l2h) {
+    cmd_injector(channel, 0xf, 0, (!l2h)<<address, 0, 0, 0, 0);
+    cmd_injector(channel, 0x1, 1<<rank, l2h<<address, 0, 0, 0, 0);
+    cdelay(100);
+}
+
+void ca_sample_prep_previous_period(int channel, int rank, int address, int l2h) {
+    cmd_injector(channel, 0xf, 0, (!l2h)<<address, 0, 0, 0, 0);
+    cmd_injector(channel, 0x1, 0, l2h<<address, 0, 0, 0, 0);
+    cmd_injector(channel, 0x2, 1<<rank, (!l2h)<<address, 0, 0, 0, 0);
+    cdelay(100);
+}
+
+int32_t _ca_results[SDRAM_PHY_ADDRESS_LINES][2];
+
+void setup_ca_results(void) {
+    int address;
+    for (address = 0; address < SDRAM_PHY_ADDRESS_LINES; address++) {
+        _ca_results[address][0] = -SDRAM_PHY_DELAYS; // right
+        _ca_results[address][1] = SDRAM_PHY_DELAYS;  // left
+    }
+}
+
+void mid_point_calc_and_set(uint8_t* success, const char* format_str, int channel,
+                            int rank, int address, int32_t index, int32_t left,
+                            int32_t right, inc_func inc, int cs) {
+    int32_t mid_point, delay;
+    printf(format_str, index, left - right);
+    mid_point = (left + right) / 2;
+    if (mid_point < 0 && cs)
+        mid_point += SDRAM_PHY_DELAYS;
+    else if (mid_point < 0)
+        mid_point = 0;
+
+    printf("Mid point:%"PRId32"\n", mid_point);
+    for (delay = 0; delay < mid_point; delay++) {
+        inc(channel, rank, address);
+    }
+    *success &= (left != UNSET_DELAY) && (right != UNSET_DELAY);
+}
+#endif // MEMORY_TYPE_DDR5
