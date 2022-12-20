c1887819309339ab53b0e486206694083d35f5ee
Antoni Pokusinski <apokusinski@internships.antmicro.com>
2022-12-20 15:57:17 +0100

    ibex: use newest version, add missing source files

--- litex_setup.py
+++ litex_setup.py
@@ -120,7 +120,7 @@ git_repos = {
     "pythondata-cpu-cv32e41p":     GitRepo(url="https://github.com/litex-hub/", clone="recursive"),
     "pythondata-cpu-cva5":         GitRepo(url="https://github.com/litex-hub/"),
     "pythondata-cpu-cva6":         GitRepo(url="https://github.com/sergachev/", clone="recursive", branch="fix/settings"),
-    "pythondata-cpu-ibex":         GitRepo(url="https://github.com/litex-hub/", clone="recursive", sha1=0xd3d53df),
+    "pythondata-cpu-ibex":         GitRepo(url="https://github.com/litex-hub/", clone="recursive"),
     "pythondata-cpu-minerva":      GitRepo(url="https://github.com/litex-hub/"),
     "pythondata-cpu-naxriscv":     GitRepo(url="https://github.com/litex-hub/", sha1=0x8b808becb503),
     "pythondata-cpu-picorv32":     GitRepo(url="https://github.com/litex-hub/"),
