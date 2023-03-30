d2bad6cd1a9dd0bbc8f8d184b6d63bf26066af34
Maciej Dudek <mdudek@antmicro.com>
2023-03-30 22:02:14 +0200

    Reset PHY on enable

--- litex/soc/software/liblitedram/ddr5_helpers.c
+++ litex/soc/software/liblitedram/ddr5_helpers.c
@@ -215,8 +215,10 @@ void set_data_module_phase(int channel, int module, int width, int phase, uint16
 }
 
 void enable_phy(void) {
+    ddrphy_CSRModule_rst_write(1);
+    cdelay(5000);
     ddrphy_CSRModule_rst_write(0);
-    cdelay(50);
+    cdelay(5000);
     ddrphy_CSRModule_enable_fifos_write(1);
 }
 
